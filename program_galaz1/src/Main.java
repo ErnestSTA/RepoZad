82b11bcfc9e94f0692eb82aa701a216122cdff21
ErnestSTA <ernestswbs@gmail.com>
2023-06-29 01:02:50 +0200

    Dodano komunikat o konieczności przygotowania numeru pesel

--- program_galaz1/src/Main.java
+++ program_galaz1/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Witamy na gałęzi 1");
+
+            System.out.println("Proszę przygotować numer pesel");
     }
 }
