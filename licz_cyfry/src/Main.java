a0a52d40ba561bee21e5d272b2a511153780a7b1
ErnestSTA <ernestswbs@gmail.com>
2023-06-28 23:46:32 +0200

    Wprowadzono powitanie dal użytkownika w programie licz_cyfry

---
+++ licz_cyfry/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Wprowadź liczbę: ");
+        int liczba = scanner.nextInt();
+
+        int iloscCyfr = obliczIloscCyfr(liczba);
+
+        System.out.println("Liczba cyfr w liczbie: " + iloscCyfr);
+    }
+
+    public static int obliczIloscCyfr(int liczba) {
+        if (liczba == 0) {
+            return 1; // Jeśli liczba to 0, zwracamy 1 jako ilość cyfr
+        }
+
+        int ilosc = 0;
+        while (liczba != 0) {
+            ilosc++;
+            liczba /= 10;
+        }
+
+        return ilosc;
+    }
+}
---
+++ licz_litery/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Wprowadź słowo: ");
+        String slowo = scanner.nextLine();
+
+        int dlugosc = slowo.length();
+
+        System.out.println("Liczba liter w słowie: " + dlugosc);
+    }
+}
