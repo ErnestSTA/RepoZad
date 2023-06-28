e6ab0660fd95543d43466afb3b87a691b1a87c26
ErnestSTA <ernestswbs@gmail.com>
2023-06-29 01:09:19 +0200

    Dodano prośbę aby urzytkownicy przygotowani zdjęcie

--- program_galaz2/src/Main.java
+++ program_galaz2/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Program do gałęzi 2");
+        System.out.println("Proszę przygotować zdjęcie do paszportu");
     }
 }
